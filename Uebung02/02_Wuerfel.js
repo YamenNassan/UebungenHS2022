b25ac43a34f0727d31897b1538ce855682e3b526
YamenNassan <yamen.nassan@students.fhnw.ch>
2022-09-30 13:39:31 +0200

    Update 02_Wuerfel.js

--- Uebung02/02_Wuerfel.js
+++ Uebung02/02_Wuerfel.js
@@ -1,30 +1,11 @@
-"use strict"
+
 
 function wuerfeln() {
     var f = [1,2,3,4,5,6];
 
-    var choice = 6*Math.random();
-
-    var index = 0 ;
-    if (choice < 1) {
-     index = 0;
-    }
-    else if (choice < 2) {
-       index = 1;
-    }
-    else if (choice < 3) {
-        index = 2;
-    }
-    else if (choice < 4) {
-        index = 2;
-    }
-    else if (choice < 5) {
-        index = 2;
-    }
-    else if (choice < 6) {
-        index = 2;
-    }
-    console.log(f[index]);
+    var choice = f.length*Math.random();
+    var index = Math.floor(choice);
+    return f[index];
 }
 
-wuerfeln();
+console.log(wuerfeln());
